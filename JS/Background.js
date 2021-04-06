bd4085218fe108489c5bcd863083d8683bb74065
Ayuda22 <69488449+Ayuda22@users.noreply.github.com>
2021-04-05 23:15:47 -0300

    Add files via upload

---
+++ JS/Background.js
@@ -0,0 +1,27 @@
+
+
+var audio = new Audio('img/trilhaSonora.mp3')
+audio.volume = 0.2
+audio.loop = true
+
+var audExplo = new Audio('img/explosion.wav')
+
+var imgHub = new Image()
+imgHub.src = "img/backgroundHub.png"
+
+var imgGame = new Image()
+imgGame.src = "img/backgroundGame.png"
+
+var imgControles = new Image()
+imgControles.src = "img/backgroundControles.png"
+
+var imgPlayer = new Image()
+imgPlayer.src = "img/megumin.png"
+
+var imgMonstro = new Image()
+imgMonstro.src = "img/monstro.png"
+
+var imgExplosao = new Image()
+imgExplosao.src = "img/explosion.png"
+
+
